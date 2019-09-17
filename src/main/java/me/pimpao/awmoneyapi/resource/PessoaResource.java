8fc8ae2bdc6c57d32d89066f20ebed450d53ff98
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2019-09-16 23:34:38 -0300

    Using events to adding location header

--- src/main/java/me/pimpao/awmoneyapi/resource/PessoaResource.java
+++ src/main/java/me/pimpao/awmoneyapi/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package me.pimpao.awmoneyapi.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import me.pimpao.awmoneyapi.event.RecursoCriadoEvent;
 import me.pimpao.awmoneyapi.model.Pessoa;
 import me.pimpao.awmoneyapi.repository.PessoaRepository;
 
@@ -27,6 +28,9 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher applicationEventPublisher;
+	
 	@GetMapping
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
@@ -34,11 +38,9 @@ public class PessoaResource {
 	
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
-		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(pessoa.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);		
+		applicationEventPublisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
 	@GetMapping(value = "/{codigo}")
